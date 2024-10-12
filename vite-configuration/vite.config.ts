8c811e0598fd8be1addb4ab84ab4b294781dbbf5
nc1z <ncxwork@gmail.com>
2024-10-12 21:52:58 +0800

    feat: configure aliases

--- vite-configuration/vite.config.ts
+++ vite-configuration/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
+});
